41fe86937fe064117db1ff99201dd6faf355412f
Jeffrey Lembeck <jlembeck@gmail.com>
2016-12-20 12:23:36 -0800

    feat: initial commit
    At this point I'm learning how the buffer works, also how vectors work,
    I really don't know anything. The world is a black box and I will wash
    it clean

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io::Read;
+use std::fs::File;
+
+fn get_file (name: &str) -> File  {
+    match File::open(name) {
+        Ok(v) => v,
+        Err(err) =>
+            panic!("Did some shit and it didn't work: {}", err)
+    }
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut f = get_file("test/security.png");
+
+    let mut buffer = Vec::new();
+    match f.read_to_end(&mut buffer) {
+        Ok(size) => size,
+        Err(err) =>
+            panic!("Tried to read entire file and gagged: {}", err)
+    };
+
+    //I 73 H 72 D 68 R 82
+    // 4 bytes width
+    // 4 bytes height
+    //
+    let length = buffer.len();
+    let header = buffer.split_off(8);
+
+    assert_eq!(header, [137, 80, 78, 71, 13, 10, 26, 10]);
+
+    for x in &buffer {
+        println!("{}", x);
+    }
 }
