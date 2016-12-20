4326278b3ce5f3ac81f54897763922d355652732
Jeffrey Lembeck <jlembeck@gmail.com>
2016-12-20 13:32:23 -0800

    hey, ashley and i paired and we wrote a test and got this a little further down the path

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,65 @@
 use std::io::Read;
 use std::fs::File;
 
-fn get_file (name: &str) -> File  {
+#[test]
+fn test_is_png () {
+    let png = get_file("./test/security.png");
+    let jpg = get_file("./test/red.jpg");
+    assert!(is_png(png), "This is a PNG");
+    assert!(!is_png(jpg), "This is not a PNG");
+}
+
+fn get_file (name: &str) -> File {
     match File::open(name) {
         Ok(v) => v,
         Err(err) =>
-            panic!("Did some shit and it didn't work: {}", err)
+            panic!("Did some shit and it didn't work: {}", err),
     }
+}
 
+fn is_png (mut filename: File) -> bool {
+    let mut buffer = vec![0; 8];
+    match filename.read_exact(&mut buffer) {
+        Ok(size) => size,
+        Err(err) =>
+            panic!("This has less than 8 bytes: {}", err),
+    };
+
+    buffer == [137, 80, 78, 71, 13, 10, 26, 10]
 }
 
 fn main() {
-    let mut f = get_file("test/security.png");
+    let mut file = get_file("./test/security.png");
+    let mut file2 = get_file("./test/security.png");
 
-    let mut buffer = Vec::new();
-    match f.read_to_end(&mut buffer) {
-        Ok(size) => size,
-        Err(err) =>
-            panic!("Tried to read entire file and gagged: {}", err)
-    };
+    if is_png(file) {
+        let mut buffer = Vec::new();
+        match file2.read_to_end(&mut buffer) {
+            Ok(size) => size,
+            Err(err) =>
+                panic!("This was bad and ugh: {}", err),
+        };
+
+        let mut iter = buffer.chunks(4);
+        for chunk in iter {
+            if chunk == [73, 72, 68, 82] {
+                let width = iter.next();
+            }
+        }
+   //     file.
+
+       // for every 8 bytes
+       //     examine bytes
+       //         when i hit
+                    //I 73 H 72 D 68 R 82
+        //            grab the deets from the next sets of bytes
+    } else {
+        panic!("NO DICE");
+    }
 
     //I 73 H 72 D 68 R 82
     // 4 bytes width
     // 4 bytes height
     //
-    let length = buffer.len();
-    let header = buffer.split_off(8);
-
-    assert_eq!(header, [137, 80, 78, 71, 13, 10, 26, 10]);
-
-    for x in &buffer {
-        println!("{}", x);
-    }
 }
+
